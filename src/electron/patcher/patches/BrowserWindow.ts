ad0fa89b4696ddb424ab606b1194d300a981d64b
Skullbite <mawler12@gmail.com>
2024-03-11 15:02:14 -0400

    Removed splash preload

--- src/electron/patcher/patches/BrowserWindow.ts
+++ src/electron/patcher/patches/BrowserWindow.ts
@@ -1,16 +1,15 @@
 import { BrowserWindow } from "electron";
 import { join } from "path";
 
-const { env, platform } = process;
+const { env } = process;
 
 export default class PatchedBrowserWindow extends BrowserWindow {
     constructor(opts: Electron.BrowserWindowConstructorOptions) {
         env.DISCORD_PRELOADER = opts.webPreferences!.preload;
         
         opts.webPreferences!.preload = join(__dirname, "preload.min.js");
-        // @ts-expect-error ITS REAL
-        if (opts.webPreferences.nativeWindowOpen) opts.webPreferences.preload = join(__dirname, "preload.min.js");
-        else opts.webPreferences!.preload = join(__dirname, "splash.min.js");
+        /*if (opts.webPreferences.nativeWindowOpen)*/ opts.webPreferences!.preload = join(__dirname, "preload.min.js");
+        // else opts.webPreferences!.preload = join(__dirname, "splash.min.js");
 
         return new BrowserWindow(opts);
 
