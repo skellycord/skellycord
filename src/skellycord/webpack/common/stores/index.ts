aceaa392f985e0e393f8aa5e1cae6a26d3f74e70
Skullbite <mawler12@gmail.com>
2024-03-12 15:25:03 -0400

    added message store to common

--- src/skellycord/webpack/common/stores/index.ts
+++ src/skellycord/webpack/common/stores/index.ts
@@ -3,3 +3,4 @@ export { ChannelStore } from "./ChannelStore";
 export { GuildStore } from "./GuildStore";
 export { SelectedChannelStore } from "./SelectedChannelStore";
 export { SelectedGuildStore } from "./SelectedGuildStore";
+export { MessageStore } from "./MessageStore";
