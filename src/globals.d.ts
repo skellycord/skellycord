046f6714eb20beb2057ac1a9afb77704e06be84f
Skullbite <mawler12@gmail.com>
2024-04-09 15:02:43 -0400

    defined `SkellycordNative` globally

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -4,6 +4,8 @@ declare global {
             jsx: import("react").CElement<any, any>;
             jsxs: import("react").CFactory<any, any>;
         }
+        // todo: types
+        SkellycordNative: any;
         DiscordNative: any;
         DiscordSentry: any;
         __SENTRY__: any;
