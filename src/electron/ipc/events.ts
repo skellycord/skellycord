922076134e548d2a2694603832ba26852e245386
Skullbite <mawler12@gmail.com>
2024-05-31 18:21:03 -0400

    removed unnecessary IPC exports

--- src/electron/ipc/events.ts
+++ src/electron/ipc/events.ts
@@ -1,4 +1,2 @@
-export const START_QUICK_CSS = "START_QUICK_CSS";
 export const READ_FILE = "READ_FILE";
 export const FILE_EXISTS = "FILE_EXISTS";
-export const WATCH_FILE = "WATCH_FILE";
--- src/electron/ipc/renderer.ts
+++ src/electron/ipc/renderer.ts
@@ -1,10 +1,5 @@
 import { ipcRenderer } from "electron";
-import { join } from "path";
 import * as events from "./events";
-
-export const fileUtils = {
-    join: join
-};
 
 export async function readFile(filePath: string): Promise<string> {
     return ipcRenderer.invoke(events.READ_FILE, filePath);
