9ce4b22896110af8f1c3ec8dafac104652d6a027
Skullbite <mawler12@gmail.com>
2024-05-31 18:17:58 -0400

    added typings for `SkellycordNative` on render

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,9 +3,12 @@ declare global {
         _react: {
             jsx: import("react").CElement<any, any>;
             jsxs: import("react").CFactory<any, any>;
-        }
+        };
         // todo: types
-        SkellycordNative: any;
+        SkellycordNative: {
+            readFile: (fn: string) => Promise<string>;
+            fileExists: (fn: string) => Promise<boolean>;
+        };
         DiscordNative: any;
         DiscordSentry: any;
         __SENTRY__: any;
