f08bafffbb601b4f58b8ad64052b94640c142502
Skullbite <git@bork.quest>
2024-06-04 10:58:46 -0400

    fixed request patch blocking (hopefully) in-client webhook reqs

--- src/electron/main/patches/requests.ts
+++ src/electron/main/patches/requests.ts
@@ -2,12 +2,18 @@ import { session } from "electron";
 
 session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders }, cb) => {
     // this was powercords btw
-    Object.keys(responseHeaders)
+    Object.keys(responseHeaders!)
         .filter(k => (/^content-security-policy/i).test(k))
-        .map(k => (delete responseHeaders[k]));
+        .map(k => (delete responseHeaders![k]));
     cb({ responseHeaders });
 });
 
-session.defaultSession.webRequest.onBeforeRequest(({ url }, cb) => cb({ 
-    cancel: /api\/v(.*)\/(science|metrics|webhooks)/g.test(url)
+const trackingRe = /api\/v(.*)\/(science|metrics)/g;
+// const sentryRe = /assets\/sentry\.(.*)\.js/g;
+
+session.defaultSession.webRequest.onBeforeRequest(({ url, method }, cb) => cb({ 
+    cancel: 
+        trackingRe.test(url) || 
+        // sentryRe.test(url) ||
+        (url.includes("webhooks") && method === "POST")
 }));
